8ff028e23804013d27ed970ceb0816a55ce1710d
subjeevan <99754701+subjeevan@users.noreply.github.com>
2023-07-05 20:33:55 +0545

    added contact, models

--- everestmomo/urls.py
+++ everestmomo/urls.py
@@ -18,7 +18,9 @@ from django.contrib import admin
 from django.urls import path,include
 from home.views import home
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/',home,name='home'),
+
 ]
--- home/models.py
+++ home/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class FoodMenu(models.Model):
+    dishname=models.CharField(max_length=100)
+    type=models.CharField(max_length=20)
+    price=models.IntegerField()
+    dishimage=models.ImageField(upload_to='dish')
+
+class Contact(models.Model):
+    fname=models.CharField(max_length=10)
+    lname=models.CharField(max_length=10)
+    email=models.EmailField()
+    mobile=models.IntegerField()  
+    message=models.CharField(max_length=50)
+
+class Review(models.Model):
+    name=models.CharField(max_length=10)
+    message=models.CharField(max_length=10)
+    date=models.DateTimeField()
+
+class Orgainfo(models.Model):
+    organame=models.CharField(max_length=50)
+    address=models.CharField(max_length=100)
+    email=models.EmailField()
+    landline=models.IntegerField()  
+    mobile=models.IntegerField()  
+    altmobile=models.IntegerField()  
+    message=models.CharField(max_length=50)
